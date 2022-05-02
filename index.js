a86d0319e5047a99de82b93bd827f252e9ae8f12
Tom Haigh <tmhaigh@gmail.com>
2022-05-02 20:42:14 +1000

    added page modes

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue!'
-  }
+window.addEventListener('DOMContentLoaded', () => {
+  const app = new Vue({
+    el: '#app',
+    data: {
+      page: 'create'
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+  });
 });
