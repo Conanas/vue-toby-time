3254b6464a80d81c0a4aff0f9a77fb3922996543
Tom Haigh <tmhaigh@gmail.com>
2022-05-02 20:50:57 +1000

    added bootstrap

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ window.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
     el: '#app',
     data: {
-      page: 'create'
+      page: 'create',
+      reps: 0,
+      restTime: 0,
+      sets: 0,
+      breakTime: 0
     },
     methods: {
 
