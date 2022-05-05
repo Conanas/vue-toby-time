8b4f877a585d2a1e22254fef6dc24889f752d6d7
Tom Haigh <tmhaigh@gmail.com>
2022-05-05 20:09:49 +1000

    added initial countdown timer and handle page change function

--- index.js
+++ index.js
@@ -2,14 +2,27 @@ window.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
     el: '#app',
     data: {
-      page: 'create',
+      pageName: 'createPage',
       reps: 1,
-      restTime: 0,
+      restTime: 1,
       sets: 1,
-      breakTime: 0
+      breakTime: 1,
+      countdown: 0
     },
     methods: {
-      
+      changePage(pageName) {
+        this.pageName = pageName;
+      },
+      startWorkout() {
+        this.countdown = this.restTime;
+        const timer = new moment.duration(1000).timer({ loop: true }, () => {
+          this.countdown--;
+          if (!this.countdown) {
+            timer.stop();
+          }
+        });
+        timer.start();
+      }
     },
     computed: {
 
