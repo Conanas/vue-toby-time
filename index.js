4dfe2d92e196f05ff7c28697792d146c2a4cb04e
Tom Haigh <tom.haigh@intygrate.com>
2022-05-14 09:58:31 +1000

    working on timerMode function

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ window.addEventListener('DOMContentLoaded', () => {
     el: '#app',
     data: {
       pageName: 'createPage',
-      repsTotal: 3,
+      repTotal: 3,
       repCount: 1,
       restTime: 3,
-      setsTotal: 3,
+      setTotal: 3,
       setCount: 1,
       breakTime: 5,
       countdown: 0,
       timer: null
     },
     created() {
-      this.timer = new moment.duration(1000).timer({ loop: true, start: false }, () => this.timerCallback());
+      this.timer = new moment.duration(1000).timer({ loop: true, start: false, wait: 0, executeAfterWait: true }, () => this.timerCallback());
     },
     methods: {
       changePage(pageName) {
@@ -22,10 +22,10 @@ window.addEventListener('DOMContentLoaded', () => {
           this.countdown = this.restTime;
         }
         if (pageName === 'createPage') {
-          this.repsTotal = 3;
+          this.repTotal = 3;
           this.restTime = 3;
           this.repCount = 1;
-          this.setsTotal = 3;
+          this.setTotal = 3;
           this.setCount = 1;
           this.breakTime = 5;
           this.countdown = 0;
@@ -38,12 +38,10 @@ window.addEventListener('DOMContentLoaded', () => {
           this.timer.stop();
           this.countdown = this.restTime;
           this.repCount++;
-          console.log('end', this.timer.isStarted())
         }
       },
       startTimer() {
         this.timer.start();
-        console.log(this.timer.isStarted());
       }
     },
     computed: {
@@ -65,24 +63,17 @@ window.addEventListener('DOMContentLoaded', () => {
         }
       },
       timerMode() {
-        if (!this.timer.isStarted() && !this.timer.isStopped() && (this.repCount === 1) && (this.setCount === 1)) {
-          return 'GO';
-        }
-
-        if (this.timer.isStopped() && (this.repCount >= 1) && (this.setCount >= 1)) {
-          return 'GO';
-        }
-
-        if (!this.timer.isStopped() && (this.repCount !== this.repsTotal) && (this.setCount !== this.setsTotal)) {
-          return 'REST';
-        }
-
-        if (!this.timer.isStopped() && (this.repCount === this.repsTotal) && (this.setCount !== this.setsTotal)) {
-          return 'BREAK';
-        }
-
-        if (!this.timer.isStarted() && (this.repCount === this.repsTotal) && (this.setCount === this.setsTotal)) {
+        console.log(this.timer)
+        if ((this.repCount === this.repTotal) && (this.setCount === this.setTotal)) {
           return 'COMPLETE';
+        } else if ((this.repCount === this.repTotal) && (this.setCount !== this.setTotal) && (!this.timer.isStopped() || this.timer.isStarted())) {
+          return 'BREAK';
+        } else if ((this.repCount !== this.repTotal) && (this.setCount !== this.setTotal) && (!this.timer.isStopped() || this.timer.isStarted())) {
+          return 'REST';
+        } else if (this.timer.isStopped() || !this.timer.isStarted()) {
+          return 'GO';
+        } else {
+          return 'GO';
         }
       }
     }
